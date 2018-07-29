2a3680133ce05753d335dbfb998962f39f9f6447
Kelsey Fecho <kfecho30@gmail.com>
2018-07-29 18:54:43 +0000

    automatically backed up by learn

--- src/Tier1.js
+++ src/Tier1.js
@@ -19,7 +19,7 @@ export default class Tier1 extends Component {
     // present in our solution. What should they be replaced with?
     return (
       <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={"#0F0"} />
+        <Tier2 color={getRandomColor()} />
         <Tier2 color={"#0FF"} />
       </div>
     )
